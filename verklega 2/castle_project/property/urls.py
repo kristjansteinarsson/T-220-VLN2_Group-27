2a6e296f639ff6cd3e6693094b7803f3319497d0
Sara <saraj24@ru.is>
2025-05-12 16:49:59 +0000

    FIXEd IT

--- verklega 2/castle_project/property/urls.py
+++ verklega 2/castle_project/property/urls.py
@@ -3,8 +3,9 @@ from django.shortcuts import redirect
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name='login'),
+    path('', views.login_view, name='login'), 
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('home/', views.home, name='home'),  
     path('accounts/login/', lambda request: redirect('login')),
 ]
--- verklega 2/castle_project/property/views.py
+++ verklega 2/castle_project/property/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 
-# Home view
-def home(request):
-    return render(request, 'home.html')
-
 def login_view(request):
+    if request.method == 'POST':
+        return redirect('home')
     return render(request, 'log_in.html')
 
 def signup_view(request):
+    if request.method == 'POST':
+        return redirect('home')
     return render(request, 'sign_up.html')
+
+def home(request):
+    return render(request, 'home.html')
